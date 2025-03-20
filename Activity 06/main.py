1cf8470f05f5a285788a413a1a55dd5661413512
shiiiii12 <167260757+shiiiii12@users.noreply.github.com>
2025-03-20 22:18:56 +0800

    Update main.py

--- Activity 06/main.py
+++ Activity 06/main.py
@@ -3,20 +3,18 @@ from item_manager import ItemManager
 def main():
     manager = ItemManager()
 
-    # Adding items (ID is randomly generated)
     manager.create_item("Laptop", "Asus Rog Strix G16", 1200.99)
     manager.create_item("Phone", "Iphone 16E", 799.49)
     
-    manager.list_items()  # Show all items
+    manager.list_items()
 
-    # Prompt user to add a new item
     name = input("Enter item name: ")
     description = input("Enter item description: ")
     price = float(input("Enter item price: "))
 
     manager.create_item(name, description, price)
     
-    manager.list_items()  # Show updated list
+    manager.list_items()
 
 if __name__ == "__main__":
     main()
